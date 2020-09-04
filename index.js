b6b5d9be3480ef8f74b5709e29a9362c9b646ea0
Aslanator <30744567+aslanator@users.noreply.github.com>
2020-09-04 15:44:43 +0300

    Add vue router (#6)
    add vue router, readme

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 import Tabs from "./components/Tabs";
 import Tab from "./components/Tab";
+import VueRouter from "vue-router";
+
+
 
 export default {
     install(Vue, options) {
+        const router = new VueRouter({
+            mode: 'history',
+        });
+        Vue.use(router);
         Vue.component('sv-tabs', Tabs);
-        Vue.component('sv-tab', Tab)
+        Vue.component('sv-tab', Tab);
     }
 }
